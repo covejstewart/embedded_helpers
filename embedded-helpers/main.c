6e169c5e9212eedcca75bb2f4c5c9ce6f35672b7
Jeff Stewart <jstewart@covetechnology.com>
2014-12-09 21:05:57 -0800

    Initial Commit

---
+++ embedded-helpers/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  embedded-helpers
+//
+//  Created by Jeffrey Stewart on 12/9/14.
+//  Copyright (c) 2014 Cove Technology. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
